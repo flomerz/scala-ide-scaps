5583377a5b6655e24cb280d1de98e10b1244113b
Royce Manavalan <royce_manavalan@hotmail.com>
2016-04-11 14:27:04 +0200

    Initial GUI for Scala Search Page

---
+++ scala-ide-scaps-plugin/src/main/scala/scaps/eclipse/ui/actions/ScalaSearchAction.scala
@@ -0,0 +1,43 @@
+package scaps.eclipse.ui.actions
+
+import org.eclipse.ui.IWorkbenchWindowActionDelegate
+import org.eclipse.jface.action.IAction
+import org.eclipse.jface.viewers.ISelection
+import org.eclipse.ui.IWorkbenchWindow
+import org.eclipse.search.internal.ui.SearchPlugin
+import org.eclipse.search.ui.NewSearchUI
+
+class ScalaSearchAction extends IWorkbenchWindowActionDelegate{
+
+  private var window: IWorkbenchWindow = _
+  private var pageID = "scaps.eclipse.ui.pages.ScalaSearchPage"
+  
+  
+  def init(window: IWorkbenchWindow){
+    this.window = window
+  }
+  
+  def run(action: IAction){
+    getWorkbenchWindow()
+    if(window.getActivePage == null){
+      print("Run: Something is not good!")
+      return
+    }
+    NewSearchUI.openSearchDialog(window,pageID)
+  }
+  
+  def selectionChanged(action: IAction, selection: ISelection){
+  }
+  
+  def dispose(){
+    window = null
+  }
+  
+  def getWorkbenchWindow() {
+    if(window == null){
+    	window = SearchPlugin.getActiveWorkbenchWindow()
+    }
+  }
+  
+
+}
---
+++ scala-ide-scaps-plugin/src/main/scala/scaps/eclipse/ui/pages/ScalaSearchPage.scala
@@ -0,0 +1,51 @@
+package scaps.eclipse.ui.pages
+
+import org.eclipse.jface.dialogs.DialogPage
+import org.eclipse.search.ui.ISearchPage
+import org.eclipse.swt.SWT
+import java.awt.Panel
+import org.eclipse.search.ui.ISearchPageContainer
+import org.eclipse.search2.internal.ui.InternalSearchUI
+import sun.security.jca.GetInstance
+import org.eclipse.swt.widgets.Composite
+import org.eclipse.swt.layout.GridLayout
+import org.eclipse.swt.widgets.Text
+import org.eclipse.swt.layout.GridData
+import org.eclipse.swt.widgets.Label
+import org.eclipse.jface.dialogs.Dialog
+
+class ScalaSearchPage extends DialogPage with ISearchPage {
+    
+  private var inputText: Text = _
+  
+
+  def createControl(parent: Composite) {
+    val result = new Composite(parent, SWT.NONE)
+    result.setFont(parent.getFont)
+    val layout = new GridLayout(2, false)
+    result.setLayout(layout)
+    
+    inputText = new Text(result, SWT.BORDER)
+    inputText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1))
+    inputText.setText("Search for functions, methods and values...")
+    
+    val exampleQueriesLabel = new Label(result, SWT.NONE)
+    exampleQueriesLabel.setText("Example Queries")
+    
+    val exampleQueriesTextLabel = new Label(result, SWT.NONE)
+    exampleQueriesTextLabel.setText("max: Int - An integer value with `max` in it's name or doc comment \n max: (Int, Int) => Int - A function taking two ints and returning Int. \n max: Int => Int => Int - Same query as above but in curried form. \n Ordering[String] - Implementations of the `Ordering` typeclass for strings. \n List[A] => Int => Option[A] - A generic query which uses a type parameter `A`. All type identifiers consisting of a single character are treated as type parameters. \n List => Int => Option - The identical query as above but with omitted type parameters. \n &~ - Searches for symbolic operators are also possible.")
+    
+    setControl(result)
+    Dialog.applyDialogFont(result)
+    print("+++++++++++++++++++++")
+  }
+
+  def performAction(): Boolean = {
+    print(inputText.getText)
+    InternalSearchUI.getInstance.runSearchInBackground(null, null)
+  }
+
+  def setContainer(container: ISearchPageContainer) {
+
+  }
+}
