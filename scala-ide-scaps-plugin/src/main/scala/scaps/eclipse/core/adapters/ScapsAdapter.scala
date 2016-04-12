02fe87c8338a791fde5a39250d7a12aec96c1a2d
Florian Merz <florian.p.m@gmail.com>
2016-04-12 10:26:32 +0200

    initialize scaps adapter with index dir path

--- scala-ide-scaps-plugin/src/main/scala/scaps/eclipse/core/adapters/ScapsAdapter.scala
+++ scala-ide-scaps-plugin/src/main/scala/scaps/eclipse/core/adapters/ScapsAdapter.scala
@@ -20,32 +20,34 @@ import scaps.scala.featureExtraction.ScalaSourceExtractor
 import scaps.searchEngine.SearchEngine
 import scaps.settings.Settings
 
-class ScapsAdapter extends StrictLogging {
+class ScapsAdapter(indexDir: String) extends StrictLogging {
   private def compiler(classPath: Seq[String]) = CompilerUtils.createCompiler(classPath)
 
   private def sourceExtractor(classPath: Seq[String]) = new ScalaSourceExtractor(compiler(classPath))
   private def libraryExtractor(classPath: Seq[String]) = new JarExtractor(compiler(classPath))
 
-  private def searchEngine(indexDir: String) = {
+  private def searchEngine = {
     var conf = Settings.fromApplicationConf.modIndex { index => index.copy(indexDir = indexDir) }
     SearchEngine(conf).get
   }
 
-  def indexProject(indexDir: String, classPath: Seq[String], projectSourcePaths: List[String]) {
+  def indexProject(classPath: Seq[String], projectSourcePaths: List[String]) {
     val sourceFiles = projectSourcePaths.map { projectSourcePath =>
       val source = Source.fromFile(projectSourcePath)(Codec.UTF8).toSeq
       new BatchSourceFile(projectSourcePath, source)
     }
-    indexDefinitions(indexDir, sourceExtractor(classPath)(sourceFiles))
+    indexDefinitions(sourceExtractor(classPath)(sourceFiles))
   }
 
-  def indexLibrary(indexDir: String, classPath: Seq[String], librarySourcePath: String) {
-    indexDefinitions(indexDir, libraryExtractor(classPath)(new File(librarySourcePath)))
+  def indexLibrary(classPath: Seq[String], librarySourcePath: String) {
+    indexDefinitions(libraryExtractor(classPath)(new File(librarySourcePath)))
   }
 
-  private def indexDefinitions(indexDir: String, definitionStream: Stream[ExtractionError \/ Definition]) {
+  def indexFinalize = searchEngine.finalizeIndex().get
+
+  private def indexDefinitions(definitionStream: Stream[ExtractionError \/ Definition]) {
     def definitions = ExtractionError.logErrors(definitionStream, logger.info(_))
-    searchEngine(indexDir).index(definitions).get
+    searchEngine.index(definitions).get
   }
 
 }
