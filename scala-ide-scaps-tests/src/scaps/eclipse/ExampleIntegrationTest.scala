a014cde7d31480e29264dc8af4e1e0bf95cf4f12
Florian Merz <florian.p.m@gmail.com>
2016-04-17 17:36:04 +0200

    improved example test

--- scala-ide-scaps-tests/src/scaps/eclipse/ExampleIntegrationTest.scala
+++ scala-ide-scaps-tests/src/scaps/eclipse/ExampleIntegrationTest.scala
@@ -1,8 +1,12 @@
 package scaps.eclipse
 
+import org.eclipse.core.resources.ResourcesPlugin
+import org.eclipse.jdt.core.IPackageFragmentRoot
+import org.eclipse.jdt.core.JavaCore
+import org.eclipse.jdt.internal.core.PackageFragment
 import org.junit.Before
 import org.junit.Test
-import org.junit.Assert._
+import org.junit.Ignore
 
 class ExampleIntegrationTest {
 
@@ -12,8 +16,19 @@ class ExampleIntegrationTest {
   }
 
   @Test
+  @Ignore
   def test1 {
-    println("iuu test1")
+    val workspace = ResourcesPlugin.getWorkspace.getRoot
+    val workspacePath = workspace.getLocation
+    val proj = workspace.getProjects.filter(_.hasNature(JavaCore.NATURE_ID)).head
+    val javaProj = JavaCore.create(proj)
+
+    val srcDirs = javaProj.getAllPackageFragmentRoots.filter(_.getKind == IPackageFragmentRoot.K_SOURCE).head.getChildren.last
+    val p: PackageFragment = srcDirs.asInstanceOf[PackageFragment]
+
+    val srcs = p.getCompilationUnits.head.getResource
+
+    println("done")
   }
 
 }
