9138adb276712049d34170d37185e16043d65832
Florian Merz <florian.p.m@gmail.com>
2016-04-19 22:41:13 +0200

    fixed scaps index dir path

--- scala-ide-scaps-plugin/src/main/scala/scaps/eclipse/ui/handlers/AbstractUCHandler.scala
+++ scala-ide-scaps-plugin/src/main/scala/scaps/eclipse/ui/handlers/AbstractUCHandler.scala
@@ -3,5 +3,5 @@ package scaps.eclipse.ui.handlers
 import org.eclipse.core.resources.ResourcesPlugin
 
 abstract class AbstractUCHandler {
-  lazy val _indexDir = ResourcesPlugin.getWorkspace.getRoot.getLocation.toString + ".metadata/scaps"
+  lazy val _indexDir = ResourcesPlugin.getWorkspace.getRoot.getLocation.toString + "/.metadata/scaps"
 }
