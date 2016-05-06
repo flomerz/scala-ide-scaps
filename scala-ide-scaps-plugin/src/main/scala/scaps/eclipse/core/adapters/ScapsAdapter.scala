6fa869c7da0fd86cd1ab8378d439f8cdb3154322
Florian Merz <florian.p.m@gmail.com>
2016-05-06 17:05:44 +0200

    scaps indexer - set module (name and path to library)

--- scala-ide-scaps-plugin/src/main/scala/scaps/eclipse/core/adapters/ScapsAdapter.scala
+++ scala-ide-scaps-plugin/src/main/scala/scaps/eclipse/core/adapters/ScapsAdapter.scala
@@ -1,24 +1,24 @@
 package scaps.eclipse.core.adapters
 
 import java.io.File
-import scala.io.Codec
 import scala.io.Source
 import scala.reflect.internal.util.BatchSourceFile
+import org.eclipse.core.resources.IFile
 import org.eclipse.core.resources.ResourcesPlugin
+import org.eclipse.jdt.core.ICompilationUnit
 import com.typesafe.scalalogging.StrictLogging
-import scalaz.{ \/ => \/ }
+import scalaz._
 import scalaz.std.stream.streamInstance
 import scaps.api.Definition
+import scaps.api.Result
+import scaps.api.ValueDef
 import scaps.scala.featureExtraction.CompilerUtils
 import scaps.scala.featureExtraction.ExtractionError
 import scaps.scala.featureExtraction.JarExtractor
 import scaps.scala.featureExtraction.ScalaSourceExtractor
 import scaps.searchEngine.SearchEngine
 import scaps.settings.Settings
-import org.eclipse.jdt.core.ICompilationUnit
-import scaps.api.Result
-import scaps.api.ValueDef
-import org.eclipse.core.resources.IFile
+import scaps.api.Module
 
 class ScapsAdapter(indexDir: String) extends StrictLogging {
   private val workspacePath = ResourcesPlugin.getWorkspace.getRoot.getLocation
@@ -33,19 +33,23 @@ class ScapsAdapter(indexDir: String) extends StrictLogging {
     SearchEngine(conf).get
   }
 
-  def indexProject(classPath: Seq[String], projectSourceUnits: Seq[ICompilationUnit]): Unit = {
-    val sourceFiles = projectSourceUnits.map { projectSourceUnit =>
-      val projectSourcePath = projectSourceUnit.getPath
-      val projectSourceAbsolutePath = workspacePath.append(projectSourcePath).toOSString
-      val codec = projectSourceUnit.getResource.asInstanceOf[IFile].getCharset
-      val source = Source.fromFile(projectSourceAbsolutePath, codec).toSeq
-      new BatchSourceFile(projectSourceAbsolutePath, source)
-    }
-    indexDefinitions(sourceExtractor(classPath)(sourceFiles.toList))
+  def indexProject(classPath: Seq[String], projectSourceUnits: Seq[ICompilationUnit]): Unit = projectSourceUnits match {
+    case Seq() =>
+    case _ =>
+      val sourceFiles = projectSourceUnits.map { projectSourceUnit =>
+        val projectSourcePath = projectSourceUnit.getPath
+        val projectSourceAbsolutePath = workspacePath.append(projectSourcePath).toOSString
+        val codec = projectSourceUnit.getResource.asInstanceOf[IFile].getCharset
+        val source = Source.fromFile(projectSourceAbsolutePath, codec).toSeq
+        new BatchSourceFile(projectSourceAbsolutePath, source)
+      }
+      val module = Module(projectSourceUnits.head.toString, "PROJECT SOURCE", "0.1.0-SNAPSHOT")
+      indexDefinitions(sourceExtractor(classPath)(sourceFiles.toList), module)
   }
 
   def indexLibrary(classPath: Seq[String], librarySourceRootFile: File): Unit = {
-    indexDefinitions(libraryExtractor(classPath)(librarySourceRootFile))
+    val module = Module(librarySourceRootFile.getAbsolutePath, librarySourceRootFile.getName, "0.1.0-SNAPSHOT")
+    indexDefinitions(libraryExtractor(classPath)(librarySourceRootFile), module)
   }
 
   def indexReset = searchEngine.resetIndexes
@@ -56,9 +60,18 @@ class ScapsAdapter(indexDir: String) extends StrictLogging {
     searchEngine.search(searchQuery, Set()).get.getOrElse(List())
   }
 
-  private def indexDefinitions(definitionStream: Stream[ExtractionError \/ Definition]): Unit = {
-    def definitions = ExtractionError.logErrors(definitionStream, logger.info(_))
-    searchEngine.index(definitions).get
+  private def indexDefinitions(extractionStream: Stream[ExtractionError \/ Definition], module: Module): Unit = {
+
+    def handleExtractionErrors(extractionStream: Stream[ExtractionError \/ Definition]): Stream[Definition] =
+      ExtractionError.logErrors(extractionStream, logger.info(_))
+
+    def withModule(definitionStream: Stream[Definition], module: Module): Stream[Definition] =
+      for (definition <- definitionStream) yield {
+        definition.withModule(module)
+      }
+
+    val definitionSteam = withModule(handleExtractionErrors(extractionStream), module)
+    searchEngine.index(definitionSteam).get
   }
 
 }
