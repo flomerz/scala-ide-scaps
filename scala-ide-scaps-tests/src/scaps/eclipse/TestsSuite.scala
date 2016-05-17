4e60444fc941b76b9ddade1c5c282de2ae5a0d61
Royce Manavalan <royce_manavalan@hotmail.com>
2016-05-17 21:31:24 +0200

    Unit test

--- scala-ide-scaps-tests/src/scaps/eclipse/TestsSuite.scala
+++ scala-ide-scaps-tests/src/scaps/eclipse/TestsSuite.scala
@@ -2,13 +2,12 @@ package scaps.eclipse
 
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
-
 import scaps.eclipse.core.adapters.ScapsAdapterIntegrationTest
-
+import scaps.eclipse.core.services.ScapsServiceUnitTest
 
 @RunWith(classOf[Suite])
 @Suite.SuiteClasses(Array(
   classOf[ExampleIntegrationTest],
-  classOf[ScapsAdapterIntegrationTest]
-))
+  classOf[ScapsAdapterIntegrationTest],
+  classOf[ScapsServiceUnitTest]))
 class TestsSuite {}
---
+++ scala-ide-scaps-tests/src/scaps/eclipse/core/services/ScapsServiceUnitTest.scala
@@ -0,0 +1,33 @@
+package scaps.eclipse.core.services
+
+import org.eclipse.core.runtime.IAdaptable
+import org.eclipse.core.runtime.IPath
+import org.eclipse.jdt.core.IClasspathEntry
+import org.eclipse.jdt.core.IJavaProject
+import org.eclipse.jdt.core.IPackageFragmentRoot
+import org.eclipse.ui.IWorkingSet
+import org.junit.Assert._
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Matchers._
+import org.mockito.Mockito._
+import org.mockito.runners.MockitoJUnitRunner
+import org.junit.Before
+import java.io.File
+import org.eclipse.jdt.internal.core.JarPackageFragmentRoot
+import org.eclipse.core.runtime.preferences.IEclipsePreferences
+
+@RunWith(classOf[MockitoJUnitRunner])
+class ScapsServiceUnitTest {
+
+  val scapsService = new ScapsService()
+  val pluginPreferences = mock(classOf[IEclipsePreferences])
+
+  @Test
+  def testExtractOfIJavaProject {
+    scapsService.setIndexerRunning(true)
+    val result = pluginPreferences.getBoolean("indexRunning")
+    assertTrue(result)
+  }
+
+}
