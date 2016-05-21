c806a7c36cc0dc3a237cdcd9d1fc1da564959131
Florian Merz <florian.p.m@gmail.com>
2016-05-21 17:47:23 +0200

    search page - formatted example text and added links to it

--- scala-ide-scaps-plugin/src/main/scala/scaps/eclipse/ui/view/search/ScapsSearchPage.scala
+++ scala-ide-scaps-plugin/src/main/scala/scaps/eclipse/ui/view/search/ScapsSearchPage.scala
@@ -23,12 +23,24 @@ import org.eclipse.core.runtime.Status
 import org.eclipse.search.ui.ISearchResultListener
 import org.eclipse.jface.resource.ImageDescriptor
 import com.typesafe.scalalogging.StrictLogging
+import org.eclipse.swt.widgets.Link
+import org.eclipse.swt.events.SelectionAdapter
+import org.eclipse.swt.events.SelectionEvent
 
 class ScapsSearchPage extends DialogPage with ISearchPage with StrictLogging {
 
   private var inputText: Text = _
 
-  def createControl(parent: Composite) {
+  private val exampleQueriesText = """<a>max: Int</a> - An integer value with `max` in it's name or doc comment
+                                     |<a>max: (Int, Int) => Int</a> - A function taking two ints and returning Int.
+                                     |<a>max: Int => Int => Int</a> - Same query as above but in curried form.
+                                     |<a>Ordering[String]</a> - Implementations of the `Ordering` typeclass for strings.
+                                     |<a>List[A] => Int => Option[A]</a> - A generic query which uses a type parameter `A`.
+                                     |All type identifiers consisting of a single character are treated as type parameters.
+                                     |<a>List => Int => Option</a> - The identical query as above but with omitted type parameters.
+                                     |<a>+</a> - Searches for symbolic operators are also possible.""".stripMargin
+
+  def createControl(parent: Composite): Unit = {
     val result = new Composite(parent, SWT.NONE)
     result.setFont(parent.getFont)
     val layout = new GridLayout(1, false)
@@ -41,27 +53,28 @@ class ScapsSearchPage extends DialogPage with ISearchPage with StrictLogging {
     inputText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1))
 
     val exampleQueriesLabel = new Label(result, SWT.NONE)
-    exampleQueriesLabel.setText("Example Queries")
+    exampleQueriesLabel.setText("Example Queries:")
 
-    val exampleQueriesTextLabel = new Label(result, SWT.NONE)
-    exampleQueriesTextLabel.setText(
-      """max: Int - An integer value with `max` in it's name or doc comment
-          max: (Int, Int) => Int - A function taking two ints and returning Int.
-          max: Int => Int => Int - Same query as above but in curried form.
-          Ordering[String] - Implementations of the `Ordering` typeclass for strings.
-          List[A] => Int => Option[A] - A generic query which uses a type parameter `A`.
-                                        All type identifiers consisting of a single character are treated as type parameters.
-          List => Int => Option - The identical query as above but with omitted type parameters.
-          &~ - Searches for symbolic operators are also possible.""")
+    val exampleQueriesTextLink = new Link(result, SWT.NONE)
+    exampleQueriesTextLink.setText(exampleQueriesText)
+    exampleQueriesTextLink.addSelectionListener(new SelectionAdapter {
+      override def widgetSelected(event: SelectionEvent): Unit = {
+        inputText.setText(event.text)
+      }
+    })
 
     setControl(result)
     Dialog.applyDialogFont(result)
   }
 
   def performAction: Boolean = {
-    NewSearchUI.runQueryInBackground(new ScapsSearchQuery(inputText.getText))
-    NewSearchUI.activateSearchResultView()
+    search(inputText.getText)
     true
+  }
+
+  private def search(text: String): Unit = {
+    NewSearchUI.runQueryInBackground(new ScapsSearchQuery(text))
+    NewSearchUI.activateSearchResultView()
   }
 
   def setContainer(container: ISearchPageContainer): Unit = {}
